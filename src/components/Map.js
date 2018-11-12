e73699de5a434d9457792c4416b07fd09ae83e38
trista <trista_mccleary@yahoo.com>
2018-11-11 22:24:53 -0500

    map setup

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
+/* Google Maps content */
 class Map extends Component {
   render() {
     return (
       <main>
-        <div id="map"></div>
+        <div id="map" role="application" aria-label="map" aria-hidden="true"></div>
       </main>
     );
   }
