13703d4041b094eeb4cd4bbf2fb9c72c02955f9b
trista <trista_mccleary@yahoo.com>
2018-11-11 22:28:58 -0500

    hamburger toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+/* Navigation Bar and toggle button */
 class NavBar extends Component {
   render() {
     return (
       <nav id="navbar">
-        <h2 id="head-text">Neighborhood Maps</h2>
+        <h1 id="header-text">Neighborhood Maps</h1>
+        <div className="hamburger-menu">
+          <button className="hamburger" aria-label="menu"><i className="fa fa-bars" onClick={this.props.toggleSideBar}/></button>
+        </div>
       </nav>
     );
   }
